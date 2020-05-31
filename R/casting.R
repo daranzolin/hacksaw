8a1301f471eecf5cfeec698520839e10f80cf933
daranzolin <daranzolin@gmail.com>
2020-05-31 00:19:50 -0700

    data to .data param

--- R/casting.R
+++ R/casting.R
@@ -1,29 +1,28 @@
 #' Cast columns to a specified data type
 #'
-#' @param data a table of data
+#' @param .data a table of data
 #' @param ... A selection of columns
 #'
 #' @export
 #' @rdname casting
 #'
 #' @examples
-#' library(dplyr)
-#' df <- tibble(x = 1:3, y = as.character(1:3), z = c(0, 0, 1))
+#' df <- data.frame(x = 1:3, y = as.character(1:3), z = c(0, 0, 1))
 #' df %>% cast_character(x)
 #' df %>% cast_numeric(y)
 #' df %>% cast_logical(z)
-cast_character <- function(data, ...) {
-  dplyr::mutate_at(data, dplyr::vars(...), as.character)
+cast_character <- function(.data, ...) {
+  dplyr::mutate_at(.data, dplyr::vars(...), as.character)
 }
 
 #' @rdname casting
 #' @export
-cast_numeric <- function(data, ...) {
-  dplyr::mutate_at(data, dplyr::vars(...), as.numeric)
+cast_numeric <- function(.data, ...) {
+  dplyr::mutate_at(.data, dplyr::vars(...), as.numeric)
 }
 
 #' @rdname casting
 #' @export
-cast_logical <- function(data, ...) {
-  dplyr::mutate_at(data, dplyr::vars(...), as.logical)
+cast_logical <- function(.data, ...) {
+  dplyr::mutate_at(.data, dplyr::vars(...), as.logical)
 }
